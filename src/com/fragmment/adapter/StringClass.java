41bf55f55bdd675f177affdc7541160e457fee16
kevthanewversi <murimiclerk@gmail.com>
2015-11-25 01:24:54 +0300

    added String class for get and set the strings itemname and itemsize

---
+++ src/com/fragmment/adapter/StringClass.java
@@ -0,0 +1,20 @@
+package com.fragmment.adapter;
+//This class is used to get and set the strings itemname and itemsize
+//For each of the textviews since its a double textview listview
+public class StringClass {
+ String one;
+ String two;
+ 
+ public void  setname(String one){
+	 this.one = one;
+ }
+ public String getname(){
+	 return one;
+ }
+ public void  setsize(String two){
+	 this.two = two;
+}
+ public String getsize(){
+	 return two;
+ }
+}
